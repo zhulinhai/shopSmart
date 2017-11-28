4eebc264cbe25a11fac503c2cb7b34aaa935dcf1
linhaizhu <zhulh128@163.com>
2017-11-28 23:11:40 +0800

    订单

--- public/js/image-dialog.js
+++ public/js/image-dialog.js
@@ -130,10 +130,10 @@
                 $("body").on('change', '#uploadImageFile',function () {
                     var file = $('#uploadImageFile').val();
                     if (file.length > 0) {
-                        uploadImageToServer('uploadImageFile','eImages', null, function (result) {
+                        uploadImageToServer('uploadImageFile','images', null, function (result) {
                             loading(false);
-                            alert(result);
                             if (parseInt(result.status) === 0) {
+                                alert("result", result.uri);
                                 dialog.find("[data-url]").val(result.uri);
                             } else {
                                 alert(result.message);

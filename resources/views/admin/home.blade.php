8274a4d5da1611469cd1fd73833d60d348cf2ed5
linhaizhu <zhulh128@163.com>
2017-11-28 23:01:38 +0800

    统计

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -10,20 +10,22 @@
             <div class="panel-title"><a><i class="glyphicon glyphicon-signal"></i> 数据统计</a></div>
         </div>
         <div class="panel-body">
-            <?php $items = array(
-                array('num'=>$counts['act'],'name'=>'活动'),
-                array('num'=>$counts['merchant'],'name'=>'商家'),
-                array('num'=>$counts['article'],'name'=>'日迹'),
-                array('num'=>$counts['member'],'name'=>'会员'),
-                array('num'=>$counts['comment'],'name'=>'总评论')
-            );
-            ?>
-            @foreach($items as $item)
-                <div class="col-sm-3 col-md-2">
-                    <h3 class="list-group-item-heading">{{ $item['num'] }}</h3>
-                    <p class="list-group-item-text">{{ $item['name'] }}</p>
-                </div>
-            @endforeach
+            <div class="row">
+                <?php $items = array(
+                    array('num'=>$counts['product'],'name'=>'活动'),
+                    array('num'=>$counts['merchant'],'name'=>'商家'),
+                    array('num'=>$counts['article'],'name'=>'日迹'),
+                    array('num'=>$counts['member'],'name'=>'会员'),
+                    array('num'=>$counts['comment'],'name'=>'总评论')
+                );
+                ?>
+                @foreach($items as $item)
+                    <div class="col-sm-3 col-md-2">
+                        <h3 class="list-group-item-heading">{{ $item['num'] }}</h3>
+                        <p class="list-group-item-text">{{ $item['name'] }}</p>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
